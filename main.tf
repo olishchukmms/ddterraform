b41c63282720ca2ebfdb1969e708b2a4e05de1f8
Andrey Olishchuk <olishchuk@mediamarktsaturn.com>
2023-01-26 12:23:39 +0100

    Dashboard

--- main.tf
+++ main.tf
@@ -6,9 +6,42 @@ terraform {
   }
 }
 
-# Configure the Datadog provider
-provider "datadog" {
-  api_key = var.DDAPIKEY
-  app_key = var.DDAPPKEY
-  api_url = "https://api.datadoghq.eu/"
+# Example Ordered Layout
+resource "datadog_dashboard" "MAAC_dashboard" {
+  title        = "Monitoring as a code"
+  description  = "Created using the Datadog provider in Terraform"
+  layout_type  = "ordered"
+  is_read_only = true
+
+  widget {
+    hostmap_definition {
+      request {
+        fill {
+          q = "source:kubernetes"
+        }
+      }
+      node_type       = "container"
+      no_group_hosts  = true
+      no_metric_hosts = true
+      style {
+        palette      = "yellow_to_green"
+        palette_flip = true
+        fill_min     = "10"
+        fill_max     = "20"
+      }
+      title = "Random widget"
+    }
+  }
+
+  widget {
+    note_definition {
+      content          = "Note text"
+      background_color = "pink"
+      font_size        = "14"
+      text_align       = "center"
+      show_tick        = true
+      tick_edge        = "left"
+      tick_pos         = "50%"
+    }
+  }
 }
